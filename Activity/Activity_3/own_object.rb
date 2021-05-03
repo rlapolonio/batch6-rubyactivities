a504d07abccaa845c487c7cc21e0014aad901780
Rhenfel Apolonio <rlapolonio@gmail.com>
2021-05-03 21:00:11 +0800

    update activity 3 folder

--- Activity/Activity_3/own_object.rb
+++ Activity/Activity_3/own_object.rb
@@ -1,38 +1,15 @@
-# class FrontLiner
-#     def initialize(name, department, position)
-#         @name = name
-#         @department = department
-#         @position = position
-#     end
+class FrontLiner
+    def initialize(name, department, position)
+        @name = name
+        @department = department
+        @position = position
+    end
 
-#     def introduce_self
-#         puts "Hi, I'm #{@name}, my position is #{@position} at #{@department} department."
-#     end
-# end
-
-# bida_hero = FrontLiner.new('Agapito', 'Emerging Diseases', 'Head Director')
-# bida_hero.introduce_self
-
-class Profile
-    attr_accessor :full_name, :work, :age
-    attr_reader :address
-    def initialize(full_name, age, address, work)
-        @full_name = full_name
-        @age = age
-        @address = address
-        @work = work
+    def introduce_self
+        puts "Hi, I'm #{@name}, my position is #{@position} at #{@department} department."
     end
 end
-  
-  my_profile = Profile.new('Juan', 18, 'Bulacan', 'Instructor')
-  
-  puts my_profile.full_name
-  my_profile.full_name = 'Juan Cruz'
-  
-  my_profile.age = 25
-  my_profile.work = 'Software Engineer'
-  
-  puts my_profile.full_name
-  puts my_profile.age
-  puts my_profile.work
-  puts my_profile.address
+
+bida_hero = FrontLiner.new('Agapito', 'Emerging Diseases', 'Head Director')
+bida_hero.introduce_self
+
